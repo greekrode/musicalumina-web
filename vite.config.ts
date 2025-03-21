766b187dade696454e9b4aec687ee300b4575441
Roderick Halim <kangritel@gmail.com>
2025-03-21 22:42:11 +0700

    Fix import

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,27 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'html-raw',
+      transform(code, id) {
+        if (id.endsWith('.html?raw')) {
+          return {
+            code: `export default ${JSON.stringify(code)}`,
+            map: null
+          };
+        }
+      }
+    }
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  assetsInclude: ['**/*.html'],
   build: {
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name?.endsWith('.html')) {
-            return 'assets/[name]-[hash][extname]';
-          }
+        assetFileNames: () => {
           return 'assets/[name]-[hash][extname]';
         }
       }
